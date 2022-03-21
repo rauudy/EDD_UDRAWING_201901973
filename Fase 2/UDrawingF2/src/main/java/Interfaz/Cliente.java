fa44d2c183d766195a2294f4111f24f35cebe4e6
RaudyCabrera <raudyc@gmail.com>
2022-03-20 22:10:12 -0600

    Logins

---
+++ Fase 2/UDrawingF2/src/main/java/Interfaz/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaz;
+
+/**
+ *
+ * @author DELL
+ */
+public class Cliente {
+    String Nombre,pass;
+    int dpi;
+
+    public Cliente(String Nombre, int dpi, String pass) {
+        this.Nombre = Nombre;
+        this.pass = pass;
+        this.dpi = dpi;
+    }
+    
+    
+}

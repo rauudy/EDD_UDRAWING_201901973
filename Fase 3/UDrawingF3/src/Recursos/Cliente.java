2339ba39fc8e45f314250d7c08b3fefd29aa9d28
RaudyCabrera <raudyc@gmail.com>
2022-04-24 23:10:39 -0600

    Inicio

---
+++ Fase 3/UDrawingF3/src/Recursos/Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+/**
+ *
+ * @author DELL
+ */
+public class Cliente {
+    long dpi;
+    long telefono;
+    String nombre;
+    String usuario;
+    String correo;
+    String contraseña;
+    String direccion;
+    String idMuni;
+
+    public Cliente() {
+        this.dpi = dpi;
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.direccion = direccion;
+        this.idMuni = idMuni;
+    }
+    
+    
+}
---
+++ Fase 3/UDrawingF3/src/Recursos/Mensajeros.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+/**
+ *
+ * @author DELL
+ */
+public class Mensajeros {
+    long dpi;
+    long telefono;
+    String nombres;
+    String licencia;
+    String genero;
+    String direccion;
+
+    public Mensajeros() {
+        this.dpi = dpi;
+        this.telefono = telefono;
+        this.nombres = nombres;
+        this.licencia = licencia;
+        this.genero = genero;
+        this.direccion = direccion;
+    }
+    
+    
+}
